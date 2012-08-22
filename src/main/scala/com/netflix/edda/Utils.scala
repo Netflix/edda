05f03cecf35d9189a69f3409bd7821ce07ff56f3
Cory Bennett <github@corybennett.org>
2012-08-21 17:09:30 -0700

    add toJson converter

--- src/main/scala/com/netflix/edda/Utils.scala
+++ src/main/scala/com/netflix/edda/Utils.scala
@@ -1,5 +1,9 @@
 package com.netflix.edda
 
+import org.codehaus.jackson.JsonGenerator
+import java.util.Date
+import org.joda.time.DateTime
+
 object Utils {
     def toObjects(args: Any*): Array[AnyRef] = {
         args.map(arg => arg match {
@@ -14,5 +18,38 @@ object Utils {
             case (v: Any, f: String) => f.format(v)
             case v: AnyRef  => v
         }).toArray[AnyRef]
+    }
+
+    def toJson(gen: JsonGenerator, obj: Any, fmt: (Any) => Any = (x: Any) => x) {
+        fmt(obj) match {
+            case v: Boolean => gen.writeBoolean(v)
+            case v: Byte => gen.writeNumber(v)
+            case v: Short => gen.writeNumber(v)
+            case v: Int => gen.writeNumber(v)
+            case v: Long => gen.writeNumber(v)
+            case v: Float => gen.writeNumber(v)
+            case v: Double => gen.writeNumber(v)
+            case v: Char => gen.writeString("" + v)
+            case v: String => gen.writeString(v)
+            case v: Date => gen.writeNumber(v.getTime)
+            case v: DateTime => gen.writeNumber(v.getMillis)
+            case v: Seq[_] => {
+                gen.writeStartArray
+                v.foreach( toJson(gen, _, fmt) )
+                gen.writeEndArray
+            }
+            case v: Map[_,_] => {
+                gen.writeStartObject
+                v.foreach( pair => {
+                    gen.writeFieldName(pair._1.toString)
+                    toJson(gen, pair._2, fmt)
+                })
+                gen.writeEndObject
+            }
+            case null => gen.writeNull
+            case v => { 
+                throw new java.lang.RuntimeException("unable to convert \"" + v + "\" to json")
+            }
+        }
     }
 }
