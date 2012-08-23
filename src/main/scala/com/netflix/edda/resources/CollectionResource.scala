02a16c431974d85b5d6262810b783cb856c36521
Cory Bennett <github@corybennett.org>
2012-08-23 14:01:50 -0700

    * tweaks to use javas.ws.rs

--- src/main/scala/com/netflix/edda/resources/CollectionResource.scala
+++ src/main/scala/com/netflix/edda/resources/CollectionResource.scala
@@ -2,11 +2,9 @@ package com.netflix.edda.resources
 
 import collection.mutable.{Set => MSet}
 
-
-import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
-import javax.ws.rs.core.MediaType
+import javax.ws.rs.{GET,Path}
+import javax.ws.rs.core.{Response,UriInfo,Context,MediaType}
 import java.io.ByteArrayOutputStream
 import java.util.Date
 
@@ -29,25 +27,30 @@ import org.slf4j.{Logger, LoggerFactory}
 
 import org.joda.time.DateTime
 
-
-class RecordCollectionServlet extends HttpServlet {
+@Path("/v2")
+class CollectionResource {
 
     private val logger = LoggerFactory.getLogger(getClass)
 
     private val factory = new MappingJsonFactory
 
-    private val collectionPathRx = """^(/[^/]+/[^/:;]+)/?([^/:;]+)?((?:;[^/;:]*(?:=[^/;:]+)?)*)(:.*)?""".r
+    private val collectionPathRx = """^([^/]+/[^/:;]+)/?([^/:;]+)?((?:;[^/;:]*(?:=[^/;:]+)?)*)(:.*)?""".r
 
-    private def fail(res: HttpServletResponse, message: String, code: Int) = {
-        res.setContentType(MediaType.APPLICATION_JSON)
-        val output = res.getOutputStream
+    private def fail(message: String, status: Response.Status): Response = {
+        val output = new ByteArrayOutputStream()
         val gen = factory.createJsonGenerator(output, UTF8)
-        res.setStatus(code);
         gen.writeStartObject
-        gen.writeNumberField("code", code)
+        gen.writeNumberField("code", status.getStatusCode)
+        gen.writeStringField("name", status.name)
         gen.writeStringField("message", message)
         gen.writeEndObject
         gen.close
+
+        Response.
+            status(status).
+            `type`(MediaType.APPLICATION_JSON).
+            entity(output.toString("UTF-8")).
+            build()
     }
 
     private def unseen(id: String, seen: MSet[String]): Boolean = {
@@ -122,7 +125,7 @@ class RecordCollectionServlet extends HttpServlet {
     }
 
     object ReqDetails {
-        def apply(req: HttpServletRequest, res: HttpServletResponse, id: String, matrixStr: String, exprStr: String): ReqDetails = {
+        def apply(req: HttpServletRequest, id: String, matrixStr: String, exprStr: String): ReqDetails = {
             val args: Map[String,String] = matrixStr match {
                 case m if m == null || m == "" => Map()
                 // skip null/or empty matrix (ie ";;a=b"), also map value null to matrix args missing value
@@ -138,11 +141,11 @@ class RecordCollectionServlet extends HttpServlet {
 
             val metaArgs   = args.filter(t => t._1.head == '_')
             val matrixArgs = args.filter(t => t._1.head != '_')
-            return new ReqDetails(req,res,id,metaArgs,matrixArgs,expr)
+            return new ReqDetails(req,id,metaArgs,matrixArgs,expr)
         }
     }
 
-    case class ReqDetails(req: HttpServletRequest, res: HttpServletResponse, id: String, metaArgs: Map[String,String], matrixArgs: Map[String,String], expr: FieldSelectorExpr) {
+    case class ReqDetails(req: HttpServletRequest, id: String, metaArgs: Map[String,String], matrixArgs: Map[String,String], expr: FieldSelectorExpr) {
         lazy val baos = {
             val v = new ByteArrayOutputStream()
             if( cb != null ) {
@@ -177,19 +180,25 @@ class RecordCollectionServlet extends HttpServlet {
         // if user requested pretty-print then reformat
         // the datetimes to be human readable, otherwise
         // use the pass-thru formatter
-        val formatter = if( pp ) Utils.dateFormatter(_) else nopFormatter(_)
-
-        def nopFormatter(arg: Any): Any = {
-            arg
-        }
+        val formatter = if( pp ) Utils.dateFormatter(_) else (x: Any) => x
 
         var timeTravelling = all || metaArgs.contains("_at") || metaArgs.contains("_since") || live
-        def send() {
+
+        def response(): Response = {
+            val builder = Response.status(Response.Status.OK);
+
             gen.close
+
             if( cb != null ) {
+                builder.`type`("application/javascript")
+                // finish off the javascript callback
                 baos.write(')')
-            }
-            baos.writeTo(res.getOutputStream)
+            } else if( diff != None ) {
+                builder.`type`(MediaType.TEXT_PLAIN)
+            } else builder.`type`(MediaType.APPLICATION_JSON)
+
+            builder.entity(baos.toString("UTF-8"))
+            builder.build()
         }
         
         private def boolArg(v: Option[String]): Boolean = {
@@ -226,33 +235,30 @@ class RecordCollectionServlet extends HttpServlet {
         Utils.toJson(details.gen, data, details.formatter)
     }
     
-    def dispatch(collName: String, details: ReqDetails) {
+    def dispatch(collName: String, details: ReqDetails): Response = {
         collName match {
             case c if CollectionManager.names.contains(c) => handleBasicCollection(collName,details)
-            case _ => fail(details.res, "invalid collection: " + collName, HttpServletResponse.SC_BAD_REQUEST)
+            case _ => fail("invalid collection: " + collName, Response.Status.BAD_REQUEST)
         }
     }
     
-    def handleBasicCollection(collName: String, details: ReqDetails): Unit = {
+    def handleBasicCollection(collName: String, details: ReqDetails): Response = {
         var recs = selectRecords(collName,details)
         if( details.id != null && !details.all) {
             if( recs.isEmpty ) {
                 if( !details.timeTravelling ) {
                     var recs = selectRecords(collName,details.copy(metaArgs=details.metaArgs ++ Map("_live" -> null, "_since" -> "0","_limit" -> "1")));
                     if( !recs.isEmpty ) {
-                        fail(details.res,"record \"" + details.id + "\" is no longer valid in collection " + collName + ". Use _at, _since or _all arguments to fetch historical records.  Last seen at " + recs.head.stime.getMillis, HttpServletResponse.SC_GONE)
-                        return;
+                        return fail("record \"" + details.id + "\" is no longer valid in collection " + collName + ". Use _at, _since or _all arguments to fetch historical records.  Last seen at " + recs.head.stime.getMillis, Response.Status.GONE)
                     }
                 }
-                fail(details.res,"record \"" + details.id + "\" not found in collection " + collName, HttpServletResponse.SC_NOT_FOUND)
-                return;
+                return fail("record \"" + details.id + "\" not found in collection " + collName, Response.Status.NOT_FOUND)
             }
         }
         else if( details.diff == None ) details.gen.writeStartArray
         if( details.diff != None && details.id != null ) {
             if( recs.size == 1 ) {
-                fail(details.res, "_diff requires at least 2 documents, only 1 found", HttpServletResponse.SC_BAD_REQUEST)
-                return
+                return fail("_diff requires at least 2 documents, only 1 found", Response.Status.BAD_REQUEST)
             }
             var diff = Utils.diffRecords(
                 recs,
@@ -272,7 +278,7 @@ class RecordCollectionServlet extends HttpServlet {
             }
         }
         if( details.diff == None && (details.id == null || details.all) ) details.gen.writeEndArray
-        details.send
+        details.response
     }
 
     def selectRecords(collName: String, details: ReqDetails): List[Record] = {
@@ -281,26 +287,22 @@ class RecordCollectionServlet extends HttpServlet {
         return unique(coll.query(query, details.limit, details.live), details)
     }
 
-    override def doGet(req: HttpServletRequest, res: HttpServletResponse) = {
-        val path = req.getRequestURI.drop(req.getServletPath.length)
+    @GET
+    @Path("{paths: .+}")
+    def getCollection(@Context req: HttpServletRequest): Response = {
+        val path = req.getRequestURI.drop(req.getContextPath.length + req.getServletPath.length + 4)
         path match {
-            case collectionPathRx(collName, id, matrixStr, exprStr) => {
-                val details = ReqDetails(req,res,id,matrixStr,exprStr)
+            case collectionPathRx(collPath, id, matrixStr, exprStr) => {
+                val collName = collPath.replace('/','.')                
+                val details = ReqDetails(req,id,matrixStr,exprStr)
                 if( details.id == null && details.diff != None ) {
-                    fail(res, "_diff argument requires use of resource id: " + req.getServletPath + collName + "/<id>", HttpServletResponse.SC_BAD_REQUEST)
+                    return fail("_diff argument requires use of resource id: " + req.getServletPath + collName + "/<id>", Response.Status.BAD_REQUEST)
                 }
                 else {
-                    if( details.cb != null ) {
-                        res.setContentType("application/javascript")
-                    }
-                    else if( details.diff != None ) {
-                        res.setContentType(MediaType.TEXT_PLAIN)
-                    }
-                    else res.setContentType(MediaType.APPLICATION_JSON)
                     dispatch(collName, details);
                 }
             }
-            case _ => fail(res, "invalid path: " + path, HttpServletResponse.SC_BAD_REQUEST)
+            case _ => fail("invalid path: " + path, Response.Status.BAD_REQUEST)
         }
     }
 }
