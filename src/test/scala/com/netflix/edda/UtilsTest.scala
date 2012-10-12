4844144f5ddc110dc3998ca071964dbb43a66634
Cory Bennett <github@corybennett.org>
2012-10-12 14:41:10 -0700

    try to force DateTime to UTC so it works on build system

--- src/test/scala/com/netflix/edda/UtilsTest.scala
+++ src/test/scala/com/netflix/edda/UtilsTest.scala
@@ -2,6 +2,7 @@ package com.netflix.edda
 
 import java.util.Properties
 import org.joda.time.DateTime
+import org.joda.time.DateTimeZone.UTC
 
 import org.scalatest.FunSuite
 
@@ -33,7 +34,7 @@ class UtilsTest extends FunSuite {
     }
 
     test("toPrettyJson") {
-        val date = new DateTime(0)
+        val date = new DateTime(0, UTC)
         val expected = """{
   "date" : "1969-12-31T16:00:00.0Z",
   "foo" : "bar",
@@ -50,7 +51,7 @@ class UtilsTest extends FunSuite {
     }
 
     test("toJson") {
-        val date = new DateTime(0)
+        val date = new DateTime(0, UTC)
         val expected = """{"date":0,"foo":"bar","list":[1,2,3,4]}"""
         expect(expected) {
             Utils.toJson(Map("foo" -> "bar", "list" -> List(1,2,3,4), "date" -> date))
@@ -64,8 +65,8 @@ class UtilsTest extends FunSuite {
     }
 
     test("diffRecords") {
-        val r1 = Record("id", 1).copy(stime=new DateTime(0))
-        val r2 = Record("id", 2).copy(stime=new DateTime(1))
+        val r1 = Record("id", 1).copy(stime=new DateTime(0, UTC))
+        val r2 = Record("id", 2).copy(stime=new DateTime(1, UTC))
         var expected = """--- collection/path/id;_pp;_at=0
 +++ collection/path/id;_pp;_at=1
 @@ -1,1 +1,1 @@
@@ -76,9 +77,9 @@ class UtilsTest extends FunSuite {
             Utils.diffRecords(Seq(r2,r1), None, "collection/path")
         }
 
-        val r3 = Record("id", List("this", "is", "a", "test")).copy(stime=new DateTime(0))
-        val r4 = Record("id", List("this", "is", "a", "great", "test")).copy(stime=new DateTime(1))
-        val r5 = Record("id", List("this", "is", "an", "even", "better", "test")).copy(stime=new DateTime(2))
+        val r3 = Record("id", List("this", "is", "a", "test")).copy(stime=new DateTime(0, UTC))
+        val r4 = Record("id", List("this", "is", "a", "great", "test")).copy(stime=new DateTime(1, UTC))
+        val r5 = Record("id", List("this", "is", "an", "even", "better", "test")).copy(stime=new DateTime(2, UTC))
 
         expected = """--- collection/path/id;_pp;_at=1
 +++ collection/path/id;_pp;_at=2
