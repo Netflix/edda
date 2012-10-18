931ee217434d2b45e7792642382a71d6cdddd808
Cory Bennett <github@corybennett.org>
2012-10-18 15:01:29 -0700

    adding license header

--- src/main/scala/com/netflix/edda/basic/BasicRecordMatcher.scala
+++ src/main/scala/com/netflix/edda/basic/BasicRecordMatcher.scala
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2012 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.netflix.edda.basic
 
 import com.netflix.edda.RecordMatcher
@@ -17,9 +32,9 @@ class BasicRecordMatcher extends RecordMatcher {
 
   protected def matchRule(rule: Any, record: Map[String, Any]): Boolean = {
     rule match {
+      case map: Map[_,_] => !map.exists(!matchRule(_, record))
       // { key: { $op1: val, $op2: val } } ==>
       // { key: { $op1: val }, key: { $op2: val } {
-      case map: Map[_,_] => !map.exists(!matchRule(_, record))
       case (key: String, value: Map[_, _]) => !value.exists(subrule => !matchRule((key -> subrule), record))
       // { $or: [ {key: value}, {key: value} ] }
       case ("$or", value: Seq[_]) => value.exists(matchRule(_,record))
