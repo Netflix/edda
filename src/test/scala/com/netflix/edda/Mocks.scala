14ca9508a2eaac570890e6066ba9c13b5f930d8d
Cory Bennett <github@corybennett.org>
2012-12-11 13:44:57 -0800

    adjust secondary cache reload algorigthm

--- src/test/scala/com/netflix/edda/Mocks.scala
+++ src/test/scala/com/netflix/edda/Mocks.scala
@@ -23,8 +23,12 @@ class TestDataStore extends DataStore {
   def init() {
   }
 
+  private def firstOf(limit: Int, records: Seq[Record]): Seq[Record] = {
+    if (limit > 0) records.take(limit) else records
+  }
+
   def query(queryMap: Map[String, Any], limit: Int, keys: Set[String], replicaOk: Boolean): Seq[Record] = {
-    records
+      firstOf(limit, records.filter(record => BasicContext.recordMatcher.doesMatch(queryMap, record.toMap)))
   }
 
   def load(replicaOk: Boolean): Seq[Record] = {
