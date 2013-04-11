db15469c80e62effb800ac83fe7e804588c1db1a
Cory Bennett <github@corybennett.org>
2013-04-11 23:40:01 +0000

    move config name settings around so they can be shared or specialized for elasticsearch datastores

--- src/main/scala/com/netflix/edda/mongo/MongoDatastore.scala
+++ src/main/scala/com/netflix/edda/mongo/MongoDatastore.scala
@@ -189,7 +189,7 @@ class MongoDatastore(val name: String) extends DataStore {
 
   lazy val primary = mongoCollection(name)
   lazy val replica = mongoCollection(name, replicaOk=true)
-  lazy val monitor = mongoCollection(Utils.getProperty("edda.mongo", "monitor.collectionName", "name", "sys.monitor").get)
+  lazy val monitor = mongoCollection(Utils.getProperty("edda", "monitor.collectionName", "mongo", "sys.monitor").get)
 
   lazy val retentionPolicy = Utils.getProperty("edda.collection", "retentionPolicy", name, "ALL")
 
