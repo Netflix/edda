c6cc23eabc98abfcb03ba37452974c02651fe89a
Ralph Tice <ralph.tice@gmail.com>
2013-11-11 18:42:58 -0600

    first draft at fixing #34 too many rds versions recorded

--- src/main/scala/com/netflix/edda/aws/AwsCollections.scala
+++ src/main/scala/com/netflix/edda/aws/AwsCollections.scala
@@ -744,6 +744,19 @@ class AwsDatabaseCollection(
                                val elector: Elector,
                                override val ctx: AwsCollection.Context) extends RootCollection("aws.databases", accountName, ctx) {
   val crawler = new AwsDatabaseCrawler(name, ctx)
+
+  /** this is overriden from [[com.netflix.edda.aws.Collection]] because we want to record
+    * changes, but not create new document revisions if the only changes are to latestRestorableTime values
+    */
+  override protected
+  def newStateTimeForChange(newRec: Record, oldRec: Record): Boolean = {
+    if (newRec == null || oldRec == null) return true
+    val newData = newRec.data.asInstanceOf[Map[String, Any]]
+    val oldData = oldRec.data.asInstanceOf[Map[String, Any]]
+    val newLatestRestorable = newRec.copy(data = newData.filterNot(_._1.startsWith("latestRestorable")))
+    val oldLatestRestorable = oldRec.copy(data = oldData.filterNot(_._1.startsWith("latestRestorable")))
+    newRec.data != oldRec.data && newLatestRestorable.dataString != oldLatestRestorable.dataString
+  }
 }
 
 /** collection for AWS ElastiCache clusters
