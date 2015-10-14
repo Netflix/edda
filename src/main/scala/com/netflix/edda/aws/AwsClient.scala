b4477a50f9b128da15c92c2de8ca847cce193145
djdrisco <david.driscoll@cfpb.gov>
2015-10-14 12:24:01 -0400

    Add proxy support to Edda  Add the ability to use Edda from behind proxy

--- src/main/scala/com/netflix/edda/aws/AwsClient.scala
+++ src/main/scala/com/netflix/edda/aws/AwsClient.scala
@@ -36,9 +36,14 @@ import com.amazonaws.services.elasticache.AmazonElastiCacheClient
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient
 import com.amazonaws.services.cloudformation.AmazonCloudFormationClient
 
+//for proxy setup
+import com.amazonaws.ClientConfiguration
+import org.slf4j.LoggerFactory
+
 object AwsClient {
+  private[this] val logger = LoggerFactory.getLogger(getClass)
   def mkCredentialProvider(accessKey: String, secretKey: String, arn: String): AWSCredentialsProvider = {
-    val provider = if( accessKey.isEmpty ) {
+    val provider = if (accessKey.isEmpty) {
       new DefaultAWSCredentialsProviderChain()
     } else {
       new AWSCredentialsProvider() {
@@ -47,76 +52,114 @@ object AwsClient {
       }
     }
     if (arn.isEmpty) {
-        provider
+      provider
     } else {
-        new STSAssumeRoleSessionCredentialsProvider(provider, arn, "edda")
+      new STSAssumeRoleSessionCredentialsProvider(provider, arn, "edda")
+    }
+  }
+  
+  def toInt(s: String): Int = util.Try(s.toInt).getOrElse(0)
+  
+  def mkClientConfiguration(): ClientConfiguration = {
+    val proxyHost = Utils.getProperty("edda", "proxyHost", "", "").get
+    val proxyPort = Utils.getProperty("edda", "proxyPort", "", "").get
+    val proxyUserName = Utils.getProperty("edda", "proxyUserName", "", "").get
+    val proxyPassword = Utils.getProperty("edda", "proxyPassword", "", "").get
+    val clientConfiguration = new ClientConfiguration()
+    if(proxyHost.isEmpty){
+      logger.info("proxyHost is Empty")
+      clientConfiguration
+    }
+    else{
+      logger.info("proxyHost is Not Empty")
+      logger.info("proxyHost value is {}",proxyHost)
+      clientConfiguration.setProxyHost(proxyHost)
+      if(!proxyPort.isEmpty){
+        logger.info("proxyPort is Not Empty")
+        clientConfiguration.setProxyPort(toInt(proxyPort))
+      }
+      if(!proxyUserName.isEmpty){
+        clientConfiguration.setProxyUsername(proxyUserName)
+      }
+      if(!proxyPassword.isEmpty){
+        clientConfiguration.setProxyPassword(proxyPassword)
+      }
+      clientConfiguration
     }
   }
 }
 
-
-/** provides access to AWS service client objects
-  *
-  * @param credentials provider used to connect to AWS services
-  * @param region used to select endpoint for AWS services
-  */
+/**
+ * provides access to AWS service client objects
+ *
+ * @param credentials provider used to connect to AWS services
+ * @param region used to select endpoint for AWS services
+ */
 class AwsClient(val provider: AWSCredentialsProvider, val region: String) {
 
-  /** uses [[http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/auth/AWSCredentials.html com.amazonaws.auth.AWSCredentials]] to create AWSCredentialsProvider
-    *
-    * @param credentials used to connect to AWS services
-    * @param region to select endpoint for AWS services
-    */
+  /**
+   * uses [[http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/auth/AWSCredentials.html com.amazonaws.auth.AWSCredentials]] to create AWSCredentialsProvider
+   *
+   * @param credentials used to connect to AWS services
+   * @param region to select endpoint for AWS services
+   */
   def this(credentials: AWSCredentials, region: String) =
-    this(new AWSCredentialsProvider() {def getCredentials = credentials; def refresh = {}}, region)
+    this(new AWSCredentialsProvider() { def getCredentials = credentials; def refresh = {} }, region)
 
-  /** create credentials from config file for account
-    * @param account
-    */
+  /**
+   * create credentials from config file for account
+   * @param account
+   */
   def this(account: String) =
     this(
       AwsClient.mkCredentialProvider(
         Utils.getProperty("edda", "aws.accessKey", account, "").get,
         Utils.getProperty("edda", "aws.secretKey", account, "").get,
-        Utils.getProperty("edda", "aws.assumeRoleArn", account, "").get
-      ),
-      Utils.getProperty("edda", "region", account, "").get
-    )
+        Utils.getProperty("edda", "aws.assumeRoleArn", account, "").get),
+      Utils.getProperty("edda", "region", account, "").get)
 
-  /** create credential from provided arguments
-    *
-    * @param accessKey for account access
-    * @param secretKey for account access
-    * @param region used to select endpoint for AWS service
-    */
+  /**
+   * create credential from provided arguments
+   *
+   * @param accessKey for account access
+   * @param secretKey for account access
+   * @param region used to select endpoint for AWS service
+   */
   def this(accessKey: String, secretKey: String, region: String) =
-    this(AwsClient.mkCredentialProvider(accessKey,secretKey, ""), region)
-
+    this(AwsClient.mkCredentialProvider(accessKey, secretKey, ""), region)
 
   /** get [[http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/ec2/AmazonEC2Client.html com.amazonaws.services.ec2.AmazonEC2Client]] object */
   def ec2 = {
-    val client = new AmazonEC2Client(provider)
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonEC2Client(provider,clientConfig)
     client.setEndpoint("ec2." + region + ".amazonaws.com")
     client
   }
 
   /** get [[http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/autoscaling/AmazonAutoScalingClient.html com.amazonaws.services.autoscaling.AmazonAutoScalingClient]] object */
   def asg = {
-    val client = new AmazonAutoScalingClient(provider)
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonAutoScalingClient(provider,clientConfig)
     client.setEndpoint("autoscaling." + region + ".amazonaws.com")
     client
   }
 
   /** get [[http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/elasticloadbalancing/AmazonElasticLoadBalancingClient.html com.amazonaws.services.elasticloadbalancing.AmazonElasticLoadBalancingClient]] object */
   def elb = {
-    val client = new AmazonElasticLoadBalancingClient(provider)
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonElasticLoadBalancingClient(provider,clientConfig)
     client.setEndpoint("elasticloadbalancing." + region + ".amazonaws.com")
     client
   }
 
   /** get [[http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/s3/AmazonS3Client.html com.amazonaws.services.s3.AmazonS3Client]] object */
   def s3 = {
-    val client = new AmazonS3Client(provider)
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonS3Client(provider,clientConfig)
     if (region == "us-east-1")
       client.setEndpoint("s3.amazonaws.com")
     else
@@ -126,7 +169,9 @@ class AwsClient(val provider: AWSCredentialsProvider, val region: String) {
 
   /** get [[http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/identitymanagement/AmazonIdentityManagementClient.html com.amazonaws.services.identitymanagement.AmazonIdentityManagementClient]] object */
   def identitymanagement = {
-    val client = new AmazonIdentityManagementClient(provider)
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonIdentityManagementClient(provider,clientConfig)
     if (region == "us-gov")
       client.setEndpoint("iam.us-gov.amazonaws.com")
     else
@@ -136,46 +181,60 @@ class AwsClient(val provider: AWSCredentialsProvider, val region: String) {
 
   /** get [[http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/sqs/AmazonSQSClient.html com.amazonaws.services.sqs.AmazonSQSClient]] object */
   def sqs = {
-    val client = new AmazonSQSClient(provider)
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonSQSClient(provider,clientConfig)
     client.setEndpoint("sqs." + region + ".amazonaws.com")
     client
   }
 
   /** get [[http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/cloudwatch/AmazonCloudWatchClient.html com.amazonaws.services.cloudwatch.AmazonCloudWatchClient]] object */
   def cw = {
-    val client = new AmazonCloudWatchClient(provider)
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonCloudWatchClient(provider,clientConfig)
     client.setEndpoint("monitoring." + region + ".amazonaws.com")
     client
   }
 
-   /** get [[http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/route53/AmazonRoute53Client.html com.amazonaws.services.route53.AmazonRoute53Client]] object */
-   def route53 = {
-      val client = new AmazonRoute53Client(provider)
-      client.setEndpoint("route53.amazonaws.com")
-      client
-   }
+  /** get [[http://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/route53/AmazonRoute53Client.html com.amazonaws.services.route53.AmazonRoute53Client]] object */
+  def route53 = {
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonRoute53Client(provider,clientConfig)
+    client.setEndpoint("route53.amazonaws.com")
+    client
+  }
 
-   def rds = {
-     val client = new AmazonRDSClient(provider)
-     client.setEndpoint("rds.amazonaws.com")
-     client
-   }
+  def rds = {
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonRDSClient(provider,clientConfig)
+    client.setEndpoint("rds.amazonaws.com")
+    client
+  }
 
-   def elasticache = {
-    val client = new AmazonElastiCacheClient(provider)
+  def elasticache = {
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonElastiCacheClient(provider,clientConfig)
     client.setEndpoint("elasticache." + region + ".amazonaws.com")
     client
-   }
+  }
 
-   def dynamo = {
-     val client = new AmazonDynamoDBClient(provider)
-     client.setEndpoint("dynamodb." + region + ".amazonaws.com")
-     client
-   }
+  def dynamo = {
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonDynamoDBClient(provider,clientConfig)
+    client.setEndpoint("dynamodb." + region + ".amazonaws.com")
+    client
+  }
 
-   def cloudformation = {
-    val client = new AmazonCloudFormationClient(provider)
+  def cloudformation = {
+    //add clientConfig parameter for ability to run from behind a proxy
+    val clientConfig = AwsClient.mkClientConfiguration()
+    val client = new AmazonCloudFormationClient(provider,clientConfig)
     client.setEndpoint("cloudformation." + region + ".amazonaws.com")
     client
-   }
+  }
 }
