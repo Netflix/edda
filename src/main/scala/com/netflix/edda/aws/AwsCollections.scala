06d67393bede421b5250eabcfee6e597916753b4
Rickard Dybeck <r.dybeck@gmail.com>
2017-01-14 01:11:41 +0100

    Crawl for EC2 NetworkInterfaces (#111)
    queryable on /aws/networkInterfaces

--- src/main/scala/com/netflix/edda/aws/AwsCollections.scala
+++ src/main/scala/com/netflix/edda/aws/AwsCollections.scala
@@ -907,3 +907,21 @@ class AwsCloudformationCollection(
                                override val ctx: AwsCollection.Context) extends RootCollection("aws.stacks", accountName, ctx) {
   val crawler = new AwsCloudformationCrawler(name, ctx)
 }
+
+/** collection for AWS Network Interfaces
+  *
+  * root collection name: aws.networkInterfaces
+  *
+  * see crawler details [[com.netflix.edda.aws.AwsNetworkInterfaceCrawler]]
+  *
+  * @param accountName account name to be prefixed to collection name
+  * @param elector Elector to determine leadership
+  * @param ctx context for AWS clients objects
+  */
+class AwsNetworkInterfaceCollection(
+                                   val accountName: String,
+                                   val elector: Elector,
+                                   override val ctx: AwsCollection.Context) extends RootCollection("aws.networkInterfaces", accountName, ctx) {
+  val crawler = new AwsNetworkInterfaceCrawler(name, ctx)
+}
+
